b079752e3c68e25bb0bfdb837602045c19e218c0
Durga-velu <durgadevi134babu@gmail.com>
2025-03-30 12:13:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //your code here
 
-document.addEventListener("dragstart",(event)=>{
+let images=document.querySelectorAll("image")
 
+images.forEach((img)=>{
 
-    event.dataTransfer.setData("image", event.target.id)
-})
-
+	img.addEventListener("dragStart",function(event){
+	event.dataTransfer.setData("image", event.target.id)	
+	});
+});
 
 document.addEventListener("dragover", function(event) {
     event.preventDefault();
@@ -15,7 +17,21 @@ document.addEventListener("dragover", function(event) {
     event.preventDefault();
    
       var data = event.dataTransfer.getData("image");
-      event.target.appendChild(document.getElementById(data));
+
+	  var draggedImg=document.getElementById("data")
+
+	  var targetImg=event.target;
+
+
+	  if(targetImg.id !=== draggedImg.id && targetImg.className==="image"){
+var change=document.createElement("div")
+event.target.parentNode.insertBefore(change,event.target)
+draggedImg.parentNode.insertBefore(event.target, draggedImage);
+
+change.parentNode.insertBefore(draggedImg,change)
+
+		  change.parentNode.removeChild(change)
+	  }
     
   });
 

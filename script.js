51b79acec9fd1a0249286779653a5f89753c0493
Durga-velu <durgadevi134babu@gmail.com>
2025-03-30 11:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
 //your code here
+
+document.addEventListener("dragstart",(event)=>{
+
+
+    event.dataTransfer.setData("image", event.target.id)
+})
+
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  });
+
+  document.addEventListener("drop", function(event) {
+    event.preventDefault();
+   
+      var data = event.dataTransfer.getData("image");
+      event.target.appendChild(document.getElementById(data));
+    
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
